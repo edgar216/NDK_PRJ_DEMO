d1eb87f79b0b180026296ef93a603516e201e235
edgar <liucj1986@yeah.net>
2015-05-05 07:00:23 +0800

    测试下  windows git

---
+++ ndk_demo_1/src/main/java/com/lcj/ndk_demo_1/HelloNDK.java
@@ -0,0 +1,21 @@
+package com.lcj.ndk_demo_1;
+
+/**
+ * Created by LCJ on 2015/5/3.
+ */
+public class HelloNDK {
+
+    /**
+     * static{}表示在loading time（载入类的时候）的时候执行下面的代码
+     */
+    static {
+
+        System.loadLibrary("HelloNDK");
+    }
+
+
+    /**
+     * 本地函数接口（java通过它调用C/C++）
+     */
+    public native String sayHello();
+}
---
+++ ndk_demo_3/src/main/jni/Adder.h
@@ -0,0 +1,8 @@
+
+
+//定义Adder类（C语言）
+typedef struct{
+
+    int (*exec)(int a, int b);
+
+}Adder_t;
